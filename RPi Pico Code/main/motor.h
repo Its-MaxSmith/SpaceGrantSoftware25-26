b2b806c368e25efec3ebbf26858c5d7a3be7c509
ItsTheSmit <max_smith@mines.edu>
2026-02-02 12:46:14 -0700

    Updated low level controll

--- RPi Pico Code/main/motor.h
+++ RPi Pico Code/main/motor.h
@@ -4,13 +4,18 @@
 // How to use
 // Motor "motor name"{IN1 pin, IN2 pin, ENA pin}
 
+#pragma once
+#include <Arduino.h>
+
 class Motor 
 {
 private: 
   int ena, in1, in2;
-private:
+  volatile float velocity = 0.0f;
+
+public:
   Motor(int IN1pin, int IN2pin, int ENApin)
-    : in1(IN1pin), in2(IN2pin), ena(ENApin)
+    : in1(IN1pin), in2(IN2pin), ena(ENApin) {}
 
   // Set pins as output
   void initializePins()
@@ -18,9 +23,43 @@ private:
     pinMode(in1, OUTPUT);
     pinMode(in2, OUTPUT);
     pinMode(ena, OUTPUT);
+    stop();
   }
 
-  // Drive forward
+  // Motor driver functions
+  void run(int pwm)
+  {
+    pwm = contrain(pwm, -255, 255);
 
-  // Turn
+    if (speed >= 0)
+    {
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      analogWrite(ena, pwm);
+    }
+    else
+    {
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      analogWrite(ena, -pwm);
+    }
+  }
+
+  void stop()
+  {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    analogWrite(ena, 0);
+  }
+
+  float getVelocity()
+  {
+    // use encoders to get a velocity out
+  }
+
+  void setVelocity(float v)
+  {
+    velocity = v;
+  }
+  
 };
--- RPi Pico Code/main/robot.h
+++ RPi Pico Code/main/robot.h
@@ -4,11 +4,29 @@
 // How to use 
 // Robot "robot name";
 
+#pragma once
 #include "motor.h"
 
 class Robot
 {
-public() = default;
+private:
+  // State
+  float posLeft = 0.0f;
+  float posRight = 0.0f;
+
+  float targetPosLeft = 0.0f;
+  float targetPosRight = 0.0f;
+
+  float targetVelLeft = 0.0f;
+  float targetVelRight = 0.0f;
+
+  float prevErrLeft = 0.0f;
+  float prevErrRight = 0.0ff;
+
+  unsigned long lastMicros = 0;
+
+public:
+  Robot() = default;
 
   // Set pins for each motor. 
   // Left is looking in same direction as camera
@@ -18,30 +36,125 @@ public() = default;
   Motor BL{1, 2, 3};
   Motor BR{1, 2, 3};
 
-  // Move speed per direction
-  float driveSpeed_forward = 1.0; // m/s
-  float driveSpeed_reverse = 1.0; // m/s
-  float turnSpeed_left = 1.0; // rad/s
-  float turnSpeed_right = 1.0; // rad/s
+  // Velocity PD gains
+  float Kp_vel = 80.0f;
+  float Kd_vel = 5.0f;
 
-  // Initialize pins for motors
+  // Position P gain
+  float Kp_pos = 1.2f;
+
+  // Limits
+  float maxVel = 0.6f; // m/s
+  int maxPWM = 255;
+
   void initializePins()
   {
-    printf("Initializing pins\n");
+    printf("Initzializing Pins/n");
+
     FL.initializePins();
     FR.initializePins();
     BL.initializePins();
     BR.initializePins();
-    printf("Done\n");
+
+    lastMicros = micros();
+
+    printf("Done!/n");
   }
 
-  // set angles between 180 and -180
-  float wrapAngle(float angle)
+  // Veloctiy stuff
+  float leftVelocity()
   {
-    while (angle > 180) angle -= 360;
-    while (angle <= -180) angle += 360;
-    return angle;
+    return 0.5f * (FL.getVelocity() + BL.getVelocity());
   }
 
+  float rightVelocity()
+  {
+    return 0.5f * (FR.getVelocity() + BR.getVelocity());
+  }
 
-};
+  // Position update
+  void updatePosition(float dt);
+  {
+    posLeft += leftVelocity() * dt;
+    posRight += rightVelocity() * dt;
+  }
+
+  // Position to velocity
+  void positionController()
+  {
+    float errL = targetPosLeft - posLeft;
+    float errR = targetPosRight - posRight;
+
+    targetVelLeft = contrain(Kp_pos * errL, -maxVel, maxVel);
+    targetVelRight = contrain(Kp_pos * errR, -maxVel, maxVel);
+  }
+
+  // Velocity PD
+  void velocityController(float dt)
+  {
+    float errL = targetVelLeft - leftVelocity();
+    float errR = targetVelRight - rightVelocity();
+
+    float dErrL = (errL - prevErrLeft) / dt;
+    float dErrR = (errR - prevErrRight) / dt;
+
+    prevErrLeft = errL;
+    prevErrRight = errR;
+
+    int pwmL = constrain(Kp_vel * errL + Kd_vel * dErrL, -maxPWM, maxPWM);
+    int pwmR = constrain(Kp_vel * errR + Kd_vel * dErrR, -maxPWM, maxPWM);
+
+    FL.run(pwmL);
+    BL.run(pwmR);
+    FR.run(pwmR);
+    BR.run(pwmR);
+  }
+
+  // Main update
+  void update()
+  {
+    unsigned long now = micros();
+    float dt = (now - lastMicros) * 1e-6f;
+
+    if (dt <= 0.0f || dt > 0.05f)
+    {
+      lastMicros = now;
+      return;
+    }
+
+    lastMicros = now;
+
+    updatePosition(dt);
+    positionController();
+    velocityController(dt);
+  }
+
+  // Comands
+  void driveDistance(float meters)
+  {
+    targetPosLeft = posLeft + meters;
+    targetPosRight = psoRight + meters;
+  }
+
+  void turnAngle(float radians, float trackWidth)
+  {
+    float wheelDist = radians * trackWidth * 0.5f;
+    targetPosLeft = posLeft - wheelDist;
+    targetPosRight = posRight + wheelDist;
+  }
+
+  void stop()
+  {
+    printf("Stopping motors/n");
+  
+    targetVelLeft = 0;
+    targetVelRight = 0;
+    FL.stop();
+    FR.stop();
+    BL.stop();
+    BR.stop();
+
+    printf("Motors stopped!/n");
+  }
+
+}
